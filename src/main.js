5fcbbc9ad4d1d2520647e79c98aab8b122e85dfc
tanjak2005 <tanjak2005@yandex.ru>
2021-08-19 21:22:17 +0300

    add posts list

--- src/main.js
+++ src/main.js
@@ -1,14 +1,19 @@
 import Post from "./components/Post/Post.js";
 import "./style.css";
 import axios from "axios";
-import config from "./config/config.js"
+import config from "./config/config.js";
+import PostsList from "./components/PostsList/PostsList.js";
 
 const app = document.querySelector('#app')
 
 axios.get(config.server + "posts").then((response) => {
-    response.data.forEach((post) => {
-        app.appendChild(
-            new Post(post).$element
-        );
-    });
+        app.appendChild(new PostsList().render());
+
+    
+
+    // response.data.forEach((post) => {
+    //     app.appendChild(
+    //         new Post(post).$element
+    //     );
+    // });
 });
