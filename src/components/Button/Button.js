82c0b51f2effa198e45923fba08ff028493088bd
Stanislav Lihodievski <grollii@mail.ru>
2021-08-23 07:58:33 +0300

    rename buttons

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,7 +1,7 @@
 import Component from "../Component.js";
 import Post from "../Post/Post.js";
 
-export default class PostsList extends Component {
+export default class Button extends Component {
   constructor(onClick, title, className = "sort-button") {
     super(className, "button");
     this.$element.addEventListener("click", onClick);
